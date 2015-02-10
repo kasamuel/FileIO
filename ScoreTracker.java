95f9aff0cab85e2aa39eb166193119fede8cae97
Karl <karl@samuelsens.com>
2015-02-09 22:18:17 -0700

    lized files, added try/catch stuff- added messages.

--- ScoreTracker.java
+++ ScoreTracker.java
@@ -1,31 +1,64 @@
 import java.util.*;
 import java.io.*;
 public class ScoreTracker{
-	
-	public static void main(String args[]){
-		//copied from previous project FOR NOW
-		TreeMap<String,String> h=new TreeMap<String,String>();
-		boolean out=true;
-		Scanner sc = new Scanner(System.in);
-		while(out){
-			System.out.println("input a number choice:\n1: Add new course\n2: Display all courses\n3: quit");
-			int i = Integer.parseInt(sc.nextLine());
-			if(i==1){
-				System.out.println("Enter the students name:");
-				String name=sc.nextLine();
-				System.out.println("Enter the students favorite course:");
-				String course=sc.nextLine();
-				h.put(name,course);
+	private ArrayList<Student> students;
+	public ScoreTracker(){
+		students=new ArrayList<Student>();
+	}
+	private void loadDataFromFile(String fileName){
+		try{
+			FileReader reader=new FileReader(fileName);
+			Scanner fil=new Scanner(reader);
+			while(fil.hasNextLine()){
+				String nam=fil.nextLine();
+				int sc=Integer.parseInt(fil.nextLine());
+				students.add(new Student(nam,sc));
 			}
-			if(i==2){
-				for(String f:h.keySet()){
-					System.out.println(f+" likes the course "+h.get(f));
-				}
-			}
-			if(i==3){
-				out=false;
-			}
+			fil.close();
 		}
-		sc.close();
+		catch(FileNotFoundException e){
+			System.out.println("ERROR: File was not found :(");
+		}
+		catch(NumberFormatException f){
+			System.out.println("ERROR: Score line was not actually a number");
+		}
+	}
+	private void printInOrder(){
+		Collections.sort(students);
+		for(Student t:students){
+			System.out.println(t);
+		}
+	}
+	public void processFiles(String[] filname){
+		for(String s:filname){
+			loadDataFromFile();
+			printInOrder();
+		}
+	}
+	public static void main(String args[]){
+		ScoreTracker track=new ScoreTracker();
+		track.processFiles("scores2.txt");
+//		boolean out=true;
+//		Scanner sc = new Scanner(System.in);
+//		while(out){
+//			System.out.println("input a number choice:\n1: Add new course\n2: Display all courses\n3: quit");
+//			int i = Integer.parseInt(sc.nextLine());
+//			if(i==1){
+//				System.out.println("Enter the students name:");
+//				String name=sc.nextLine();
+//				System.out.println("Enter the students favorite course:");
+//				String course=sc.nextLine();
+//				h.put(name,course);
+//			}
+//			if(i==2){
+//				for(String f:h.keySet()){
+//					System.out.println(f+" likes the course "+h.get(f));
+//				}
+//			}
+//			if(i==3){
+//				out=false;
+//			}
+//		}
+//		sc.close();
 	}
 }
--- Student.java
+++ Student.java
@@ -1,5 +1,17 @@
-public class Student{
-	public Student(){
-		
+public class Student implements Comparable<Student>{
+	private String nam;
+	private int score;
+	public Student(String n, int e){
+		nam=n;
+		score=e;
+	}
+	public int getScore(){
+		return score;
+	}
+	public String toString(){
+		return nam+"has a score of "+score;
+	}
+	public int compareTo(Student t){
+		return this.nam.compareTo(t.nam);
 	}
 }
